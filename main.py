314d4134a8fa992593efe3db6b9e457907b2ebf2
SLEEPERSUNLESS <155083732+SLEEPERSUNLESS@users.noreply.github.com>
2025-03-11 13:00:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-choice = input("What would you like? (espresso/latte/cappuccino): ")
-coffee = CoffeeMaker()
+coffee_maker = CoffeeMaker()
 menu = Menu()
-currency = MoneyMachine()
+money_machine = MoneyMachine()
 
-if choice == "report":
-    print(coffee.report())
+while True:
+    choice = input(f"What would you like? ({menu.get_items()}exit): ").lower()
+
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    elif choice == "exit":
+        print("Shutting down. Have a great day!")
+        break
+
+    else:
+        drink = menu.find_drink(choice)
+        if drink:
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
